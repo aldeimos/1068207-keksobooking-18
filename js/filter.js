d65ff83cddd93595fe4aaa3c0d4fd2f88ebf1492
Alexander <al.deimosfrontend@gmail.com>
2019-10-17 18:50:33 +0300

    rework

--- js/filter.js
+++ js/filter.js
@@ -22,60 +22,49 @@
       return val > 50000;
     }
   };
-  var roomsValidationMap = {
-    '1': function (val) {
-      return val === 1;
-    },
-    '2': function (val) {
-      return val === 2;
-    },
-    '3': function (val) {
-      return val === 3;
-    }
-  };
-  var guestsValidationMap = {
-    '0': function (val) {
-      return val === 0;
-    },
-    '1': function (val) {
-      return val === 1;
-    },
-    '2': function (val) {
-      return val === 2;
-    }
-  };
   var pins = [];
+
+  var filterHouseType = function (item) {
+    if (upperHouseFilter.value === DEFAULT_FILTER_VALUE) {
+      return true;
+    }
+    return item.offer.type === upperHouseFilter.value;
+  };
+  var filterPrice = function (item) {
+    if (upperPriceFilter.value === DEFAULT_FILTER_VALUE) {
+      return true;
+    }
+    return priceValidationMap[upperPriceFilter.value](item.offer.price);
+  };
+  var filterRooms = function (item) {
+    if (upperRoomsFilter.value === DEFAULT_FILTER_VALUE) {
+      return true;
+    }
+    return item.offer.rooms === Number(upperRoomsFilter.value);
+  };
+  var filterGuests = function (item) {
+    if (upperGuestsFilter.value === DEFAULT_FILTER_VALUE) {
+      return true;
+    }
+    return item.offer.guests === Number(upperGuestsFilter.value);
+  };
+  var filterFeatures = function (item) {
+    var activeInputsValue = Array.from(document.querySelectorAll('.map__checkbox:checked')).map(function (checkbox) {
+      return checkbox.value;
+    });
+    return activeInputsValue.every(function (val) {
+      return item.offer.features.includes(val);
+    });
+  };
+
   var onTypeFilterChange = function () {
     removeAllCards();
-    var filteredArray = pins.filter(function (item) {
-      if (upperHouseFilter.value === DEFAULT_FILTER_VALUE) {
-        return true;
-      }
-      return item.offer.type === upperHouseFilter.value;
-    }).filter(function (item) {
-      if (upperPriceFilter.value === DEFAULT_FILTER_VALUE) {
-        return true;
-      }
-      return priceValidationMap[upperPriceFilter.value](item.offer.price);
-    }).filter(function (item) {
-      if (upperRoomsFilter.value === DEFAULT_FILTER_VALUE) {
-        return true;
-      }
-      return roomsValidationMap[upperRoomsFilter.value](item.offer.rooms);
-    }).filter(function (item) {
-      if (upperGuestsFilter.value === DEFAULT_FILTER_VALUE) {
-        return true;
-      }
-      return guestsValidationMap[upperGuestsFilter.value](item.offer.guests);
-    }).filter(function (item) {
-      var checkedValue = Array.from(document.querySelectorAll('.map__checkbox:checked')).map(function (checkbox) {
-        return checkbox.value;
-      });
-      return checkedValue.every(function (val) {
-        return item.offer.features.includes(val);
-      });
-    });
-    renderPins(filteredArray);
+    var filteredPins = pins.filter(filterHouseType).
+    filter(filterPrice).
+    filter(filterRooms).
+    filter(filterGuests).
+    filter(filterFeatures);
+    renderPins(filteredPins);
   };
   var successHandler = function (array) {
     pins = array;
